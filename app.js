d5222564509fa202b30996b064b6c67041038900
Shawn25678 <119130434+Shawn25678@users.noreply.github.com>
2026-09-08 16:11:41 -0500

    1.0.22

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "poe2-exile-ledger-v1";
-const APP_VERSION = "1.0.21";
+const APP_VERSION = "1.0.22";
 const FEEDBACK_ISSUE_URL = "https://github.com/Shawn25678/SSEv1/issues/new";
 
 const FILTERS = [
